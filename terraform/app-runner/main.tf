9f9daf1d0abf0271277f5047dc6264ede6057b14
Angel Rivera <ariv3ra@gmail.com>
2021-06-11 15:21:13 -0400

    Terraform Org name fix

--- terraform/app-runner/main.tf
+++ terraform/app-runner/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "datapunks"
+    organization = "" # Enter the Terraform Cloud Organization here
     workspaces {
       name = "app-aws-circleci"
     }
