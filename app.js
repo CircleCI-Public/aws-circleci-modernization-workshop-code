3fc6cdde6f6bb2b68d28fb39085013a41f4ab45c
George <george@launchdarkly.com>
2021-11-09 13:02:16 -0800

    rebasing and pushing new changes

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+require('dotenv').config()
 var express = require('express');
 var app = express();
 var exports = module.exports = {};
+const LaunchDarkly = require('launchdarkly-node-server-sdk');
+
+const client = LaunchDarkly.init(process.env.LAUNCHDARKLY_SDK_KEY)
+
+const user = {
+    "key": "circleci",
+  };
+
 
 function welcomeMessage(){
     var message = "Welcome to CI/CD 101 using CircleCI!";
@@ -10,10 +19,28 @@ function welcomeMessage(){
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    // var message = "Hello World";
-    res.render("index", {message: welcomeMessage()});
+let showFeature = false
+ client.once("ready", () => {
+    client.variation("circleci-workshop", user, false,
+      (err, sf) => {
+        showFeature = sf
+        app.get('/', function (req, res) {
+            var message = "Welcome to CI/CD 101 using CircleCI & LaunchDarkly!";
+            var base_case = "Welcome to CI/CD 101 using CircleCI.";
+            res.render("index", {message: showFeature ? message : base_case});
+        });
+  })
 });
+
+
+
+client.on('update', () => {
+    client.variation("circleci-workshop", user, false,
+      (err, sf) => {
+        showFeature = sf
+  })
+  });
+
 
 var server = app.listen(5000, function () {
     console.log("Node server running...");
